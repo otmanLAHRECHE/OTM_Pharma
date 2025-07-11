3688fde012d79be4d887f4ba213d837b8ac3d753
otmane lahreche <otmane.lahreche@gmail.com>
2025-07-11 20:30:52 +0100

    update

--- OTM_Pharma_CORE/OTM_Pharma_CORE/settings.py
+++ OTM_Pharma_CORE/OTM_Pharma_CORE/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'OTM_Admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
