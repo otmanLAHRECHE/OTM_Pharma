4ee91f5e76b7b40e14853d32e690f01ddbc21602
otmane lahreche <otmane.lahreche@gmail.com>
2025-07-14 19:06:24 +0100

    update

--- OTM_Pharma_CORE/stock/models.py
+++ OTM_Pharma_CORE/stock/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class TimeStampedModel(models.Model):
+    """
+    Abstract base model that provides self-updating created and modified fields.
+    
+    Attributes:
+        created (DateTimeField): When the record was first created (auto-set)
+        last_modified (DateTimeField): When the record was last updated (auto-updated)
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Manufacturer(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=True, max_length=50)
+    country = models.CharField(null=True, blank=True, max_length=50)
+    address = models.CharField(null=True, blank=True, max_length=50)
+
+    class Meta:
+        abstract = True
+
+
+class cls(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=True, null=False, blank=False)
+    
+
